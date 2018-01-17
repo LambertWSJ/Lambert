a74f4b33d4ff2f0a3ab7cc6aae1e9d0c00e42575
= <=>
2018-01-17 18:27:15 +0800

    Switch Scene

--- src/SkyHook/Main.java
+++ src/SkyHook/Main.java
@@ -5,14 +5,17 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 
 public class Main extends Application  {
 
-    Button btn;
+    Stage window;
+    Scene scene1,scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -22,34 +25,40 @@ public class Main extends Application  {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-        primaryStage.setTitle("Title of the Window");
-
-        btn = new Button();
-        btn.setText("ClickMe");
+        window = primaryStage;
 
 
+        // Button1
+        Label label1 = new Label("Welcome to the scene");
+        Button button1 = new Button("Go to Scene2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        // Anonymous class
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("You click again");
-            }
-        });
-
-        // Lambda expression
-        btn.setOnAction(e -> {
-            System.out.println("Clicked ");
-        });
+        // Layout1
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1,button1);
+        scene1 = new Scene(layout1,200,300);
 
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(btn);
+        // Button2
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Button button2 = new Button("Go Back Scene1");
+        button2.setOnAction(event -> window.setScene(scene1));
+
+
+        // Layout2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().addAll(button2);
+        scene2 = new Scene(layout2, 500 ,600);
+
+        window.setScene(scene1);
+        window.setTitle("Javafx");
+        window.show();
+
+
+
+
+
+
 
 
 
