8ea3bce3ef532ec9ca85087b4000009c76d526cb
= <=>
2018-01-19 15:53:49 +0800

    Embedding Layouts  - HBox  - VBox  - BorderPane

--- src/SkyHook/Main.java
+++ src/SkyHook/Main.java
@@ -4,7 +4,10 @@ import Components.ConfirmationBox;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 // import components
@@ -22,45 +25,40 @@ public class Main extends Application  {
 
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
+        // 水平清單
+        HBox top_menu = new HBox();
+        Button file_button = new Button("File");
+        Button edit_button = new Button("Edit");
+        Button view_button = new Button("View");
+        top_menu.getChildren().addAll(file_button,edit_button,view_button);
+
+        // 垂直清單
+        VBox left_menu = new VBox();
+        Button btnD = new Button("D");
+        Button btnE = new Button("E");
+        Button btnF = new Button("F");
+        left_menu.getChildren().addAll(btnD,btnE,btnF);
+
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(top_menu);
+        borderPane.setLeft(left_menu);
+
+        Scene scene = new Scene(borderPane,450,500);
+
+
         window = primaryStage;
         window.setTitle("SkyHook");
-
-        // 發出關閉事件的Request
-        window.setOnCloseRequest(event -> {
-            // 阻止Request事件傳播，以免選了NO還是繼續執行Request
-            event.consume();
-
-            // 詢問是否要關閉
-            closeProgram();
-        });
-
-        button = new Button("Close program");
-        button.setOnAction(event->{
-
-            this.closeProgram();
-
-        });
-
-
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        Scene scene = new Scene(layout,450,450);
         window.setScene(scene);
         window.show();
 
-    }
-
-    private void closeProgram(){
-        Boolean answer = ConfirmationBox.display("Title","Sure you want to exit?");
-
-        if(answer)
-            window.close();
 
     }
+
+
 
 }
